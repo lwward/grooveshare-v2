ad8613e4cdcd59d0a05a84c283d84e903e27b8ee
0x6C77 <0x6c77@gmail.com>
2016-07-12 22:48:33 +0100

    Added slug pushState

--- client/static/js/main.js
+++ client/static/js/main.js
@@ -12,7 +12,7 @@ $(function() {
 
     var tmplChannelList = '<ul class="channels container row gutters">\
                                 {{#each .}}\
-                                    <li class="channel col span_6" data-channel-id="{{ _id }}">\
+                                    <li class="channel col span_6" data-channel-id="{{ _id }}" data-channel-slug="{{ slug }}">\
                                         <a href="#">\
                                             <div class="channel-image" style="background-image: url(\'/images/channels/{{ _id }}.jpg\');"></div>\
                                             <h3>{{ title }}</h3>\
@@ -34,6 +34,33 @@ $(function() {
 
         var channelList = tmplChannelList(channels);
         $container.html(channelList);
+
+        // Click handlers
+        $('.channels .channel:not(.channel--add-new) a').on('click', function(e) {
+            e.preventDefault();
+            var $this = $(this),
+                $parent = $(this).parent();
+            $parent.siblings().addClass('channel--remove');
+            $parent.addClass('channel--active');
+            $this.children('.channel-image').append($('<div>', {'class': 'loader'}));
+
+            var title = $this.children('h3').text(),
+                slug = $parent.data('channel-slug');
+
+            setTimeout(function() {
+                $parent.css({'position': 'absolute', 'top': $parent.offset().top, 'left': $parent.offset().left, 'width': $parent.width(), 'height': $parent.height()});
+                $parent.siblings().remove();
+                setTimeout(function() {
+                    $parent.css({'left': ($(window).width() / 2) - ($parent.width() / 2)});
+                }, 10);
+
+                document.title = title + ' | Grooveshare';
+                window.history.pushState({"url": slug, "pageTitle": document.title}, "", slug);
+            }, 50);
+
+            // Connect to channel
+            socket.emit('channel.join', $parent.data('channel-id'));
+        });
 
         setTimeout(function() {
             $('.channels').addClass('active');
