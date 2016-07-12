891e8b1717240743876d06ac2ccf8079b7be4967
0x6C77 <0x6c77@gmail.com>
2016-07-12 22:44:57 +0100

    Added slug to channel schema

--- models/Channel.js
+++ models/Channel.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose'),
     autopopulate = require('mongoose-autopopulate'),
     Track = require('./Track.js'),
     ShortId = require('mongoose-shortid-nodeps'),
+    utils = require('../lib/utils.js'),
     Schema = mongoose.Schema;
 
 // define the schema for our user model
 var channelSchema = mongoose.Schema({
     _id         : ShortId,
     title       : { type: String, index: true },
+    slug        : { type: String, index: true },
     owner       : { type: String },
     created     : { type: Date, default: Date.now },
     public      : Boolean,
@@ -15,5 +17,11 @@ var channelSchema = mongoose.Schema({
 });
 
 channelSchema.plugin(autopopulate);
+
+channelSchema.pre('save', function(next){
+    this.slug = utils.generateSlug(this.title);
+
+    next();
+});
 
 module.exports = mongoose.model('channel', channelSchema);
